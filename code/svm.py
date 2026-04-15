505456d2d8409c23808bbbb26ae91d07a061be99
Dominic <dmv1167@rit.edu>
2026-04-15 11:42:29 -0400

    Enhanced error handling and documentation

--- code/svm.py
+++ code/svm.py
@@ -1,78 +1,131 @@
+import os
 import pickle
 import sys
+from time import perf_counter
 
-from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score, confusion_matrix, \
-    ConfusionMatrixDisplay
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
 from sklearn.svm import OneClassSVM
 import matplotlib.pyplot as plt
 import pandas as pd
 
-"""
-Train a support vector machine based on a given dataframe
-"""
-def train_svm(df):
-    y = df.iloc[:, -1]
+TRAIN_FULL = "train_pca_kddcup_full.csv"
+TRAIN_10 = "train_pca_kddcup_10_percent.csv"
+TEST_FULL = "test_pca_kddcup_full.csv"
+TEST_10 = "test_pca_kddcup_10_percent.csv"
+MODEL_FILE = "models/svm_model.pkl"
+
+def output_timing(seconds):
+    """
+    Transforms seconds into a human-readable string
+    :param seconds: Number of seconds
+    :return: Human-readable string
+    """
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    result = ""
+    result += f"{int(hours)} hours" if hours > 0 else ""
+    result += f", {int(minutes)} minutes" if minutes > 0 else ""
+    result += f", {int(seconds)} seconds"
+    return result
+
+
+def train_svm(file_path):
+    """
+    Train a support vector machine based on a given data-file
+    :param file_path: Data-file
+    :return: Trained model
+    """
+    print(f"Training SVM on {file_path}")
+    try:
+        df = pd.read_csv(file_path)
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        return
+    if not verify_save_location(MODEL_FILE):
+        print(f"Model file not found: {MODEL_FILE}")
+        return
+
     X = df.iloc[:, :-1]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
     svm_model = OneClassSVM(kernel='rbf', gamma='auto')
-    svm_model.fit(X_train, y_train)
+    svm_model.fit(X)
 
     return svm_model
 
 def test_svm(svm_model, df):
+    """
+    Test given model against given test-data
+    :param svm_model: Pre-trained model
+    :param df: Dataframe containing test-data
+    :return: test labels and predicted labels
+    """
     y = df.iloc[:, -1]
     X = df.iloc[:, :-1]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    y_pred = svm_model.predict(X_test)
-    return y_test, y_pred
+    y_pred = svm_model.predict(X)
+    return y, y_pred
 
+def verify_save_location(filename):
+    """
+    Verify a location can be written to
+    :param filename: Path to test
+    :return: Whether the location can be written to
+    """
+    try:
+        with open(filename, 'wb') as _:
+            return True
+    except FileNotFoundError:
+        return False
 
 def save_svm(svm_model, filename):
+    """
+    Serialize a model to a file
+    :param svm_model: Model to save
+    :param filename: Destination file
+    :return: None
+    """
     with open(filename, 'wb') as file:
         pickle.dump(svm_model, file)
 
 def load_svm(filename):
+    """
+    Load a model from a file
+    :param filename: File to load
+    :return: Loaded model
+    """
     with open(filename, 'rb') as file:
         return pickle.load(file)
 
 def main():
     if len(sys.argv) < 2 or len(sys.argv) > 4 or  sys.argv[1] not in ["train", "test"]:
-        print("Usage: python svm.py <train|test> [--full] <model_file>")
+        print("Usage: python svm.py <train|test> [--full] [model_file]")
         return
+
+    base_dir = os.path.dirname(os.path.abspath(__file__))
 
     file_index = 2
     if len(sys.argv) == 4 and sys.argv[2] == "--full":
-        file_path = "Data/preprocessed/preprocessed_kddcup_full.csv"
+        file_path = os.path.join(base_dir, '..', 'Data', 'preprocessed', TRAIN_FULL if sys.argv[1] == "train" else TEST_FULL)
         file_index += 1
     else:
-        file_path = "/home/dvinciulla/Documents/ML/Project/Data/preprocessed/preprocessed_kddcup_10_percent.csv"
+        file_path = os.path.join(base_dir, '..', 'Data', 'preprocessed', TRAIN_10 if sys.argv[1] == "train" else TEST_10)
 
     if sys.argv[1] == "train":
-        print("Training SVM...")
-        try:
-            df = pd.read_csv(file_path)
-        except FileNotFoundError:
-            print(f"File not found: {file_path}")
-            return
-        model = train_svm(df)
-        save_svm(model, "../models/svm_model.pkl")
+        start = perf_counter()
+        model = train_svm(file_path)
+        print(output_timing(perf_counter() - start))
+        save_svm(model, MODEL_FILE)
     else:
         print(f"Testing {sys.argv[file_index]}")
         df = pd.read_csv(file_path)
-        model = load_svm(sys.argv[file_index])
+        model = load_svm(os.path.join(base_dir, sys.argv[file_index]))
         y_test, y_pred = test_svm(model, df)
-        print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
-        print(f"Recall: {recall_score(y_test, y_pred)}")
-        print(f"Precision: {precision_score(y_test, y_pred)}")
-        print(f"F1: {f1_score(y_test, y_pred)}")
         cm = confusion_matrix(y_test, y_pred)
         disp = ConfusionMatrixDisplay(confusion_matrix=cm)
         disp.plot()
         plt.show()
+        print("Classification Report:")
+        print(classification_report(y_test, y_pred))
 
 
 
